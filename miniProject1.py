20a34091443dbea62c5b18f29dcd45aae16a37fa
SamGuven <guvens@southwestern.edu>
2025-06-11 10:28:53 -0500

    Update

--- miniProject1.py
+++ miniProject1.py
@@ -1,11 +1,11 @@
 from pylab import *
 
-def initialize(pb0, pn0):
+def initialize(pb0, pn0, pbn0, pnb0):
 
     if(pb0 > 1 or pn0 > 1 or pb0 < 0 or pn0 < 0 or pn0 + pb0 > 1 or pn0 + pb0 < 0):
         raise ValueError("Error values not (0 ≤ pb ≤ 1; 0 ≤ pn ≤ 1; 0 ≤ pb + pn ≤ 1)")
 # Produced Error if given values are not between certain numbers
-    global pb, pn, pbresult, pnresult
+    global pb, pn, pbn0, pnb0, pbresult, pnresult
 
     pb = pb0
     pn = pn0
@@ -20,11 +20,9 @@ def observe():
 
 def update():
 
-    global pb, pn, pbresult, pnresultg
+    global pb, pn, pbresult, pnresultg, pnb, pbn
 
-    nextx = 0.5 * x + y
-    nexty = -0.5 * x + y
-    x, y = nextx, nexty
+
 
 for x0 in arange(0, 1, 0.5):
     for y0 in arange(-2, 2, 0.5):
