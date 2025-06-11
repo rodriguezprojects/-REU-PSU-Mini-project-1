c6f85cf4c69029bd9e0158e5c60a1cbb1943efa3
SamGuven <guvens@southwestern.edu>
2025-06-11 11:20:45 -0500

    Push

--- miniProject1.py
+++ miniProject1.py
@@ -2,13 +2,16 @@ from pylab import *
 
 def initialize(pb0, pn0, pbn0, pnb0):
 
-    if(pb0 > 1 or pn0 > 1 or pb0 < 0 or pn0 < 0 or pn0 + pb0 > 1 or pn0 + pb0 < 0):
+    if(not(0 <= pb0 <= 1) or not(0 <= pn0 <= 1) or pn0 + pb0 != 1
+    or not(0 <= pbn0 <= 1) or not(0 <= pnb0 <= 1)):
         raise ValueError("Error values not (0 ≤ pb ≤ 1; 0 ≤ pn ≤ 1; 0 ≤ pb + pn ≤ 1)")
 # Produced Error if given values are not between certain numbers
-    global pb, pn, pbn0, pnb0, pbresult, pnresult
+    global pb, pn, pbn, pnb, pbresult, pnresult
 
     pb = pb0
     pn = pn0
+    pbn = pbn0
+    pnb = pnb0
 
     pbresult = [pb]
     pnresult = [pn]
@@ -20,20 +23,22 @@ def observe():
 
 def update():
 
-    global pb, pn, pbresult, pnresultg, pnb, pbn
+    global pb, pn, pbresult, pnresult, pnb, pbn
 
-    pbnext = 
+    pbnext = pb * (1 - pbn) + (1 - pb) * pnb
 
-    pnnext =
+    pnnext = pn * (1 - pnb) + (1 - pn) * pbn
 
+    pb = pbnext
 
+    pn = pnnext
 
-for x0 in arange(0, 1, 0.5):
-    for y0 in arange(-2, 2, 0.5):
-        initialize(x0, y0)
-        for t in range(30):
-            update()
-            observe()
-        plot(xresult, yresult, 'b')
+for pbn0 in arange(0, 1, 0.05):
+    for pnb0 in arange(0, 1, 0.05):
+        initialize(0.3, 0.7, pnb0, pnb0)
+            for t in range(30):
+                update()
+                observe()
+                plot(pbresult, pnresult, 'b')
 
 show()
